c1cec121248db3c941857bd07dd91bfeff0882ea
legoroblox15 <littlecmason@yahoo.com>
2014-07-16 18:15:59 -0400

    my python code
    It's awsome

---
+++ madlib.py
@@ -0,0 +1,16 @@
+noun1 = input("Name a noun ")
+adjectiveer1 = input("Name an adjective ending in er ")
+verb1 = input("Name a verb ")
+name1 = input("Name a name ")
+verbing1 = input("Name a verb ending in ing ")
+noun2 = input("Name a noun ")
+name2 = input("Name a name ")
+preposition1 = input("Name a preposition ")
+verb2 = input("Name a verb ")
+adjectiveest1 = input("Name a adjective ending in est ")
+emotion1 = input("Name an emotion ")
+print("One day " + name1 + " was " + verbing1 + " " + preposition1 + " a(n) " + noun1 + " and decided that " + name2 + " and I should have a competition where we have to " + verb1 + " " + preposition1 + " a(n) " + noun1 + " and then " + verb2 + " " + preposition1 + " a(n) " + noun2 + " for 5 minutes and whoever could do it the " + adjectiveest1 + " wins. So " + name1 + " texted " + name2 + " about the competition and asked him/her if (s)he wanted to compete. Sure! " + name2 + " said as (s)he rode her bike to " + name1 + "'s house. " + name2 + " was " + emotion1 + " that (s)he was invited. as soon as (s)he made it " + name1 + " told " + name2 + " that (s)he looks " + adjectiveer1 + " than normal. " + name2 + " said that (s)he's like that because it makes her " + verb1 + " faster. " + name1 + " was intimidated because of that, but was still confident. they started the compitition. It was a close call, but " + name1 + " won!\n>")
+
+       
+    
+    
