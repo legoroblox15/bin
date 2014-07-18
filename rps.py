6c407225c7d9c38d0c0d18dc1f6d74ddf073ffa5
legoroblox15 <littlecmason@yahoo.com>
2014-07-17 20:06:46 -0400

    better things
    I fixed my error on my r.p.s program and changed my mad lib

--- rps.py
+++ rps.py
@@ -1,9 +1,14 @@
-plarps = input("Rock, paper, sissors?\n> ")
-plarps = "plarps".strip().lower()
-if plarps == "rock":
-elif plarps == "paper":
-elif plarps == "sissors":
-else:
-    print("Not an option, please try agian")
-    break
+while True:
+    plarps = input("Rock, paper, sissors?\n> ")
+    
+    if plarps == "rock":
+        print(plarps + " vs " + corps)
+    elif plarps == "paper":
+        print("hi")
+    elif plarps == "sissors":
+        print("hi")
+    else:
+        print("Not an option, please try agian")
+        
+        
 
