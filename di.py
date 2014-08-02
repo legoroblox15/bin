cafe8cc2e1066f2cdd0ab1d264173b0accfb7f93
legoroblox15 <littlecmason@yahoo.com>
2014-08-02 00:48:48 -0400

    Tweaks
    I changed randrange to randint to make it more appropriate for ints.

--- di.py
+++ di.py
@@ -2,7 +2,7 @@ import random
 numb = 0
 while True:
     numb = numb + 1
-    roll = random.randrange(1,1001)
+    roll = random.randint(1,1000)
     pr = "{}. {}".format(numb,roll)
     print(pr)
     if roll == 1:
