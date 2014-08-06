306f1364b8d15fe0fc7e8df21d3df234e1204ae5
legoroblox15 <littlecmason@yahoo.com>
2014-08-06 17:17:25 -0400

    stuff
    just stuff

--- di.py
+++ di.py
@@ -2,7 +2,7 @@ import random
 numb = 0
 while True:
     numb = numb + 1
-    roll = random.randint(1,1000000)
+    roll = random.randint(1,100)
     if roll == 1:
         trys = "It took {} try(s) to get the number 1".format(numb)
         print(trys)
