c2cc01a9910df13304011c019a7e0daa03859a38
legoroblox15 <littlecmason@yahoo.com>
2014-10-17 20:53:14 -0400

    IT'S A PARABOX!!!!
    well not yet, just the makings, if you don't know what a parabox is,
    just search 'paradox' on google images and find the cube

--- hangman.py
+++ hangman.py
@@ -3,13 +3,51 @@ import words
 print('Loading random word...')
 randomword = words.randomword()
 
+def printman():
+    wordnumber = 0
+    wordletters = ''
+    
+    for letter in randomword:
+        wordletters = wordletters + '_ '
+        
+    for letter in wordletters:
+        wordnumber = wordnumber + 1
+    wordnumber = wordnumber / 2
+
+    head = 'O'
+    bodyone = '+'
+    bodytwo = '|'
+    bodythree = ' -   -   -'
+    leftarm = '-'
+    rightarm = '-'
+    rightleg =' |'
+    leftleg = '|'
+    man = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [wordletters]
+    ]
+    for row in man:
+        for col in row:
+            print(col, end = '   ')
+        print('')
+##############################################
 while True:
+    printman()
     letter = input('>>> ').strip().lower()
     if letter == randomword:
-        print('you guessed the word')
+        print('you guessed the word!')
+        break
+    elif letter == '':
+        print('false')
     elif letter in randomword:
         print('correct')
     elif letter == 'i give up':
         print('The word was: ' + randomword)
+        break
     else:
         print('false')
+
---
+++ mymod.py
@@ -0,0 +1,10 @@
+# these are things that I wanted to add for turtles
+import turtle
+
+def randomcolor():
+    import random
+    return (random.random(), random.random(), random.random())
+
+    
+    
+
